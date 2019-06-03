4ce7f0efecf1fc6ce85548766f3b356926298204
prasanthipenmadu <prasanthipl@gmail.com>
2019-06-03 16:18:45 -0400

    Tests passed

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,28 @@
 package com.zipcodewilmington.assessment1.part5;
 
 public class Palindrome {
-
-    public Integer countPalindromes(String input){
-        return null;
+    public boolean ispal(String input,int st,int end){
+        if(st==end)
+            return true;
+        for(int i=0;i<=(end-st)/2;i++){
+            if(input.charAt(i+st)!=input.charAt(end-i))
+                return false;
+        }return true;
     }
+
+    public Integer countPalindromes(String input) {
+
+       int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = input.length() - 1; j >= i; j--) {
+                if (input.charAt(i) == input.charAt(j)) {
+                    if (ispal(input,i,j))
+                        count++;
+
+                }
+            }
+        }
+        return count;
+    }
+
 }
