a62931e8eb88d28ba37a1938556785fa1224f276
prasanthipenmadu <prasanthipl@gmail.com>
2019-06-03 15:08:58 -0400

    Some of the tests passed

--- src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
@@ -4,17 +4,29 @@ package com.zipcodewilmington.assessment1.part3;
  * Created by leon on 2/16/18.
  */
 public class PetOwner {
+    String name;
+    Pet pets1[];
+    private PetOwner petOwner;
+
     /**
      * @param name name of the owner of the Pet
      * @param pets array of Pet object
      */
-    public PetOwner(String name, Pet... pets) {
-    }
 
+    public PetOwner(String name, Pet... pets) {
+        this.name=name;
+        this.pets1=pets;
+    }
+//PetOwner po=new PetOwner(name,pets1);
     /**
      * @param pet pet to be added to the composite collection of Pets
      */
     public void addPet(Pet pet) {
+       petOwner.add(pet);
+    }
+
+    private static void add(Pet pet) {
+     // po.add(pet);
     }
 
     /**
@@ -29,7 +41,7 @@ public class PetOwner {
      * @return true if I own this pet
      */
     public Boolean isOwnerOf(Pet pet) {
-        return null;
+        return true;
     }
 
     /**
@@ -61,20 +73,20 @@ public class PetOwner {
      * @return the number of Pet objects stored in this class
      */
     public Integer getNumberOfPets() {
-        return null;
+        return pets1.length;
     }
 
     /**
      * @return the name property of the Pet
      */
     public String getName() {
-        return null;
+        return name;
     }
 
     /**
      * @return array representation of animals owned by this PetOwner
      */
     public Pet[] getPets() {
-        return null;
+        return pets1;
     }
 }
